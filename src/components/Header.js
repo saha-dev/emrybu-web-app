e09864757a26e2a5632b0bc29182730a709b8d6c
saha <acidoralkali@gmail.com>
2024-06-15 17:18:57 +0300

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import { IoMenu } from 'react-icons/io5';
+// import { IoMenu } from 'react-icons/io5';
+// import { FiMenu } from 'react-icons/fi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { RiSearchLine } from 'react-icons/ri';
 import style from './Header.module.css';
 
 function Header() {
     return (
         <div className={style.header}>
-            <IoMenu className={style.buttons} />
+            <GiHamburgerMenu className={style.buttons} />
             <div className={style.logo}></div>
             <RiSearchLine className={style.buttons} />
         </div>
