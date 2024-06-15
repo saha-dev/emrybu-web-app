2f8abcd0dfe9e88efea2c6e61b5a037a8c778086
saha <acidoralkali@gmail.com>
2024-06-15 21:13:01 +0300

    --tg-theme-bg-color

--- src/App.js
+++ src/App.js
@@ -2,18 +2,10 @@ import './App.css';
 import MenuList from './components/MenuList';
 import Header from './components/header/Header';
 
-let tg;
-
-try {
-    tg = window.Telegram.WebApp;
-} catch {}
-
-if (tg !== undefined) {
-    tg.expand();
-
-    tg.MainButton.textColor = '#FFFFFF';
-    tg.MainButton.color = '#2cab37';
-}
+const tg = window.Telegram.WebApp;
+tg.expand();
+// tg.MainButton.textColor = '#FFFFFF';
+// tg.MainButton.color = '#2cab37';
 
 function App() {
     return (
