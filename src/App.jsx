7223374149a74ae6fa8c77a59c70945908f7157d
saha <acidoralkali@gmail.com>
2024-06-23 00:19:16 +0300

    Context + buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import Home from './components/home/Home';
+import Home from './components/home/Home.jsx';
 // import MenuItems from './components/menu/MenuItems';
 import './App.css';
 import SingleMenuItems from './components/menu/SingleMenuItems';
